f1b93e6e3bd16968d21a24cd0bbf814309c788e4
iespinoza421 <espinozamonteroisaac@gmail.com>
2023-07-19 23:06:05 -0600

    Clase Main y Prueba #1

---
+++ src/main/java/com/proyecto/Usuario.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.proyecto;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Isaac
+ */
+public class Usuario {
+    
+    public void datos(){
+        
+        String Nombre =JOptionPane.showInputDialog("Digite su nombre:");
+        JOptionPane.showMessageDialog(null, "Hola" + Nombre);
+        
+    }
+    
+    
+}
