b2c8f8c9b54e5e38834ef65e6941c04c760807e0
JeikelArce <jeikelarce17@gmail.com>
2023-07-23 16:56:34 -0600

    Atributos y metodos clase Nivel

--- src/main/java/com/proyecto/Nivel.java
+++ src/main/java/com/proyecto/Nivel.java
@@ -8,6 +8,59 @@ package com.proyecto;
  *
  * @author Usuario
  */
+
 public class Nivel {
-    
+    private String nombre;
+    private int pistas;
+    private int cantidadNumerosMostrados;
+    private int cantidadNumerosCuadro;
+    private int cantidadNumerosFilaColumna;
+
+    public Nivel(String nombre, int pistas, int cantidadNumerosMostrados, int cantidadNumerosCuadro, int cantidadNumerosFilaColumna) {
+        this.nombre = nombre;
+        this.pistas = pistas;
+        this.cantidadNumerosMostrados = cantidadNumerosMostrados;
+        this.cantidadNumerosCuadro = cantidadNumerosCuadro;
+        this.cantidadNumerosFilaColumna = cantidadNumerosFilaColumna;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPistas() {
+        return pistas;
+    }
+
+    public void setPistas(int pistas) {
+        this.pistas = pistas;
+    }
+
+    public int getCantidadNumerosMostrados() {
+        return cantidadNumerosMostrados;
+    }
+
+    public void setCantidadNumerosMostrados(int cantidadNumerosMostrados) {
+        this.cantidadNumerosMostrados = cantidadNumerosMostrados;
+    }
+
+    public int getCantidadNumerosCuadro() {
+        return cantidadNumerosCuadro;
+    }
+
+    public void setCantidadNumerosCuadro(int cantidadNumerosCuadro) {
+        this.cantidadNumerosCuadro = cantidadNumerosCuadro;
+    }
+
+    public int getCantidadNumerosFilaColumna() {
+        return cantidadNumerosFilaColumna;
+    }
+
+    public void setCantidadNumerosFilaColumna(int cantidadNumerosFilaColumna) {
+        this.cantidadNumerosFilaColumna = cantidadNumerosFilaColumna;
+    }
 }
