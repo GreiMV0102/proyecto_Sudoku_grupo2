cf1b169fdaa6e967425115bd19158da3d4d8a7f5
JeikelArce <jeikelarce17@gmail.com>
2023-08-27 12:31:29 -0600

    Clase Helper cambios
    Se agregaron mas funciones a la clase Helper y se le cambio el nombre a la inicial en mayúscula a la clase y las menciones en otras clases

--- src/main/java/com/proyecto/helper.java
+++ src/main/java/com/proyecto/helper.java
@@ -3,24 +3,42 @@ package com.proyecto;
 import java.awt.TextArea;
 import javax.swing.JOptionPane;
 
-public class helper {
-    public int recibeInt(String mensaje){
+public class Helper {
+    public int recibeInt(String mensaje) {
         return Integer.parseInt(JOptionPane.showInputDialog(mensaje));
     }
-    public char recibeChar(String mensaje){
+
+    public char recibeChar(String mensaje) {
         return JOptionPane.showInputDialog(mensaje).toUpperCase().charAt(0);
     }
-    public String recibeString(String mensaje){
+
+    public String recibeString(String mensaje) {
         return JOptionPane.showInputDialog(mensaje);
     }
-    public void imprimeMensaje(String mensaje){
+
+    public void imprimeMensaje(String mensaje) {
         JOptionPane.showMessageDialog(null, mensaje);
-    }
-    public void imprimeMensaje(TextArea mensaje){
-        JOptionPane.showMessageDialog(null, mensaje);
-        
-        
     }
 
-            
+    public void imprimeMensaje(TextArea mensaje) {
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
+
+    public boolean confirmarAccion(String mensaje) {
+        int respuesta = JOptionPane.showConfirmDialog(null, mensaje, "Confirmar Acción", JOptionPane.YES_NO_OPTION);
+        return respuesta == JOptionPane.YES_OPTION;
+    }
+
+    public int elegirOpcion(String mensaje, String[] opciones) {
+        return JOptionPane.showOptionDialog(null, mensaje, "Seleccionar Opción",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opciones, opciones[0]);
+    }
+
+    public void mostrarMensajeError(String mensaje) {
+        JOptionPane.showMessageDialog(null, mensaje, "Error", JOptionPane.ERROR_MESSAGE);
+    }
+
+    public void mostrarMensajeInformacion(String mensaje) {
+        JOptionPane.showMessageDialog(null, mensaje, "Información", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
